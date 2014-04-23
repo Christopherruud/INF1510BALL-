438ae90f9cbebc56260bd23e097ff40ea9f38a93
ChristopherRuud <dispater42@gmail.com>
2014-04-23 13:35:11 +0200

    Adding constants

--- Ball20exec/Ball20exec.ino
+++ Ball20exec/Ball20exec.ino
@@ -4,8 +4,25 @@
 #include <SoftwareSerial.h>
 
 /** Framework and pseudocode for the Ball2.0 project 
-
-**/
+ * Much of this code is derived from examples in various
+ * libraries,for instance the TinyGPS++ library and so on.
+ * 
+ **/
 
 
 //Initialize constants for pins
+static const int GPSRXPin = 3, GPSTXPin = 2;
+static const uint32_t GPSBaud = 4800;
+
+//Initialize variables for stuff
+
+
+void setup() {
+
+}
+
+
+void loop() {
+
+}
+
