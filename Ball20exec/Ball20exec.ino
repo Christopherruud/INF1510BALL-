deabd711cdc799932f5557b4cb5e6509d4dab1ed
Christopherruud <dispater42@gmail.com>
2014-05-20 14:56:40 +0200

    Update Ball20exec.ino
    Added Libraries reference

--- Ball20exec/Ball20exec.ino
+++ Ball20exec/Ball20exec.ino
@@ -9,7 +9,9 @@
 /** Framework and pseudocode for the Ball2.0 project 
  * Much of this code is derived from examples in various
  * libraries,for instance the TinyGPS++ library and so on.
- * 
+ * Libraries used that are not included in the main 
+ * Arduino IDE are available for download from within the
+ * GITHUB project folder libraries/
  **/
 
 
